a681c0d198b5ce41a292ad9bb3c807d41cb31e16
Ngostsh <polypterus_delhezy_from_congo@yahoo.co.jp>
2019-11-21 18:54:41 +0900

    ユーザーページ等を実装

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
     delete 'reviews/:id' => 'reviews#destroy'
     get 'reviews/edit/:id' => 'reviews#edit'
     patch 'reviews/:id' => 'reviews#update'
+    get 'users/:id' => 'users#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
