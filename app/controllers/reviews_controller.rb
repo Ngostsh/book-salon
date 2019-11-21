b63fbeb22d6f8a6028e7a1f06f66d0b032b2c5cd
Ngostsh <polypterus_delhezy_from_congo@yahoo.co.jp>
2019-11-21 15:13:43 +0900

    書評編集機能等の実装

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -15,9 +15,38 @@ class ReviewsController < ApplicationController
     Review.create(review_params)
   end
 
+  def destroy
+    @review = Review.find(params[:id])
+    if @review.user_id == current_user.id
+      @review.destroy
+      redirect_to root_path, notice: '書評を削除しました'
+    else
+      redirect_to root_path, alert: 'ユーザーでないため、書評の削除に失敗しました。'
+    end
+  end
+
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    if @review.user_id == current_user.id
+      @review.update(review_update_params)
+      redirect_to root_path, notice: '書評を更新しました'
+    else
+      redirect_to root_path, alert: 'ユーザーでないため、書評の更新に失敗しました。'
+    end
+  end
+
   private
   def review_params
     params.permit(:name, :image, :text)
+  end
+
+  private
+  def review_update_params
+    params.require(:review).permit(:name, :image, :text).merge(user_id: current_user.id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
     get 'reviews' => 'reviews#index'
     get 'reviews/new'  =>  'reviews#new'
     post  'reviews'      =>  'reviews#create'
-    get 'reviews/:id' => 'reviews#show' 
+    get 'reviews/:id' => 'reviews#show'
+    delete 'reviews/:id' => 'reviews#destroy'
+    get 'reviews/edit/:id' => 'reviews#edit'
+    patch 'reviews/:id' => 'reviews#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
