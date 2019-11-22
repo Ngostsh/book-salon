35372dee0ff894dfb123561fb83ab03dd64a8ef2
Ngostsh <polypterus_delhezy_from_congo@yahoo.co.jp>
2019-11-22 19:38:36 +0900

    マイグレーションファイル編集

--- db/migrate/20191120111628_add_user_id_to_reviews.rb
+++ db/migrate/20191120111628_add_user_id_to_reviews.rb
@@ -1,4 +1,4 @@
-class AdduserIdToReviews < ActiveRecord::Migration[5.2]
+class AddUserIdToReviews < ActiveRecord::Migration[5.2]
   def change
     add_column :reviews, :user_id, :integer, null: false, foreign_key: true
   end
