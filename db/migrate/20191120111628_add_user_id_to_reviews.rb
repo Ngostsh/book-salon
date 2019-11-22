aec36c4310892d8d00b997bc2df35b3562e50d5f
Ngostsh <polypterus_delhezy_from_congo@yahoo.co.jp>
2019-11-22 19:32:46 +0900

    本番環境のマイグレーションでエラーが出るためマイグレーションファイルを編集

--- db/migrate/20191120111628_add_user_id_to_reviews.rb
+++ db/migrate/20191120111628_add_user_id_to_reviews.rb
@@ -1,5 +1,5 @@
 class ChangeColumnToReviews < ActiveRecord::Migration[5.2]
   def change
-    change_column :reviews, :user_id, :integer, null: false, foreign_key: true
+    add_column :reviews, :user_id, :integer, null: false, foreign_key: true
   end
 end
