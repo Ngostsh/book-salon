c9b3e9eebca2c199a6ef0b23d5dd854dc926b871
Ngostsh <polypterus_delhezy_from_congo@yahoo.co.jp>
2019-11-28 23:59:35 +0900

    reviewsコントローラー編集

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,12 @@
 class ReviewsController < ApplicationController
+  before_action :set_review, only: [:show, :destroy, :edit, :update]
+  before_action :correct_referer, only: [:show, :destroy, :edit, :update]
 
   def index
     @reviews = Review.order("created_at DESC").page(params[:page]).per(8)
   end
 
   def show
-    @review = Review.find(params[:id])
     @user = @review.user
   end
 
@@ -38,37 +39,47 @@ class ReviewsController < ApplicationController
   end
 
   def destroy
-    @review = Review.find(params[:id])
     if @review.user_id == current_user.id
       @review.destroy
       redirect_to root_path, notice: '書評を削除しました'
     else
-      redirect_to root_path, alert: 'ユーザーでないため、書評の削除に失敗しました。'
+      redirect_to root_path, alert: '書評の削除に失敗しました'
     end
   end
 
   def edit
-    @review = Review.find(params[:id])
+    if @review.user_id != current_user.id
+      redirect_to root_path, alert: '投稿ユーザーではないため、編集できません'
+    end
   end
 
   def update
-    @review = Review.find(params[:id])
     if @review.user_id == current_user.id
       @review.update(review_update_params)
       redirect_to root_path, notice: '書評を更新しました'
     else
-      redirect_to root_path, alert: 'ユーザーでないため、書評の更新に失敗しました。'
+      redirect_to root_path, alert: '投稿ユーザーではないため、書評の更新に失敗しました'
     end
   end
 
   private
+
   def review_params
     params.permit(:name, :image, :text).merge(user_id: current_user.id)
   end
 
-  private
   def review_update_params
     params.require(:review).permit(:name, :image, :text).merge(user_id: current_user.id)
+  end
+  
+  def set_review
+    @review = Review.find(params[:id])
+  end
+
+  def correct_referer
+    if request.referer.nil?
+      redirect_to root_path, alert: 'url直打ちはご遠慮下さい'
+    end
   end
 
 end
