b646aa171dde213751a0c2c1329b4cf60818898f
Ngostsh <polypterus_delhezy_from_congo@yahoo.co.jp>
2019-12-02 21:10:58 +0900

    fontawesome配置

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -3,3 +3,9 @@ document.addEventListener('turbolinks:load', function(){
     $("ul").slideToggle(200);
   });
 });
+
+document.addEventListener('turbolinks:load', function(){
+  $(".menu_button2").click(function(){
+    $("ul").slideToggle(200);
+  });
+});
