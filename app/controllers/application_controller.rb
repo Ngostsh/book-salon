fdea690b930167529aeb8357fe12e5254a4a1553
Ngostsh <polypterus_delhezy_from_congo@yahoo.co.jp>
2019-12-08 12:30:53 +0900

    ユーザー情報編集機能を実装し、ユーザー情報に画像、プロフィールを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,9 @@ class ApplicationController < ActionController::Base
 
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name])
+    added_attrs = [ :nick_name, :profile, :image, :email, :password, :password_confirmation　]
+    devise_parameter_sanitizer.permit(:sign_up, keys: [added_attrs])
+    devise_parameter_sanitizer.permit(:account_update, keys: [added_attrs])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [added_attrs])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  mount_uploader :image, ImageUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -7,6 +8,7 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,128}+\z/i
   validates :nick_name, presence: true, length: { maximum: 20}
+  validates :profile, presence: true, length: { maximum: 1000}
   validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX }, confirmation: true
   validates :email, {presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }}
 end
